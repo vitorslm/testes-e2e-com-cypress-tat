ae8275e343e7599628002612a8fd7354cf8559b5
Vitor Sumiyoshi Lima Mukai <vitor.mukai@NMAC-1448.local>
2021-09-21 11:02:39 -0300

    Create e2e testes with cypress

---
+++ cypress/integration/login.spec.js
@@ -0,0 +1,11 @@
+// cypress/integration/login.spec.js
+
+it('successfully logs in', () => {
+  cy.intercept('GET', '**/notes').as('getNotes')
+
+  cy.login(Cypress.env('USER_EMAIL'), Cypress.env('USER_PASSWORD'), {
+    cacheSession: false,
+  })
+
+  cy.wait('@getNotes')
+})
