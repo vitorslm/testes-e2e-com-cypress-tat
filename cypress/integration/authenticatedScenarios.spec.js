370beba83a56b0fcb3cd519e0478f4bdf84f0e28
Vitor Sumiyoshi Lima Mukai <vitor.mukai@NMAC-1448.local>
2021-09-21 11:11:26 -0300

    fix eslint

--- cypress/integration/authenticatedScenarios.spec.js
+++ cypress/integration/authenticatedScenarios.spec.js
@@ -1,52 +1,52 @@
 // authenticatedScenarios.spec.js
 
-describe("Scenarios where authentication is a pre-requirement", () => {
+describe('Scenarios where authentication is a pre-requirement', () => {
   beforeEach(() => {
-    cy.intercept("GET", "**/notes").as("getNotes");
-    cy.login();
-  });
+    cy.intercept('GET', '**/notes').as('getNotes')
+    cy.login()
+  })
 
-  it("CRUDs a note", () => {
-    const faker = require("faker");
-    const noteDescription = faker.lorem.words(4);
+  it('CRUDs a note', () => {
+    const faker = require('faker')
+    const noteDescription = faker.lorem.words(4)
 
-    cy.createNote(noteDescription);
-    cy.wait("@getNotes");
+    cy.createNote(noteDescription)
+    cy.wait('@getNotes')
 
-    const updatedNoteDescription = faker.lorem.words(4);
-    const attachFile = true;
+    const updatedNoteDescription = faker.lorem.words(4)
+    const attachFile = true
 
-    cy.editNote(noteDescription, updatedNoteDescription, attachFile);
-    cy.wait("@getNotes");
+    cy.editNote(noteDescription, updatedNoteDescription, attachFile)
+    cy.wait('@getNotes')
 
-    cy.deleteNote(updatedNoteDescription);
-    cy.wait("@getNotes");
-  });
+    cy.deleteNote(updatedNoteDescription)
+    cy.wait('@getNotes')
+  })
 
-  it("successfully submits the form", () => {
-    cy.intercept("POST", "**/prod/billing").as("paymentRequest");
+  it('successfully submits the form', () => {
+    cy.intercept('POST', '**/prod/billing').as('paymentRequest')
 
-    cy.fillSettingsFormAndSubmit();
+    cy.fillSettingsFormAndSubmit()
 
-    cy.wait("@getNotes");
-    cy.wait("@paymentRequest").then((response) => {
-      expect(response.state).to.equal("Complete");
-    });
-  });
+    cy.wait('@getNotes')
+    cy.wait('@paymentRequest').then((response) => {
+      expect(response.state).to.equal('Complete')
+    })
+  })
 
-  it.only("logs out", () => {
-    cy.visit("/");
-    cy.wait("@getNotes");
+  it.only('logs out', () => {
+    cy.visit('/')
+    cy.wait('@getNotes')
 
     if (
-      Cypress.config("viewportWidth") < Cypress.env("viewportWidthBreakpoint")
+      Cypress.config('viewportWidth') < Cypress.env('viewportWidthBreakpoint')
     ) {
-      cy.get(".navbar-toggle.collapsed").should("be.visible").click();
+      cy.get('.navbar-toggle.collapsed').should('be.visible').click()
     }
 
     /* ==== Generated with Cypress Studio ==== */
-    cy.get(".nav > :nth-child(2) > a").click();
-    cy.get("#email").should("be.visible");
+    cy.get('.nav > :nth-child(2) > a').click()
+    cy.get('#email').should('be.visible')
     /* ==== End Cypress Studio ==== */
-  });
-});
+  })
+})
